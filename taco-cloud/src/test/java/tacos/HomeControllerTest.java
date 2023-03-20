77705695a7f65875fce748c869f09caf57d29f84
Sadriddin Suyarov <75208220+sadriddin-suyarov@users.noreply.github.com>
2023-03-21 00:30:34 +0500

    2-chapter: chapter fully finished (#2)
    * 2-chapter:
     - content copied from 1-chapter through master branch;
     - changed project base package name to 2-chapter;
    
    * 2-chapter:
     - added lombok dependency to pom.xml;
     - created Ingredient.java domain class;
    
    * 2-chapter:
     - created DesignTacoController.java controller;
    
    * 2-chapter:
     - created design.html view;
    
    * 2-chapter:
     - created OrderController.java controller and its orderForm.html view;
     - temporal stub in design.html view repalaced with actual value;
    
    * 2-chapter:
     - added validation dependency;
     - validation applied on views and controllers;
    
    * master:
     - removed package associated with chapter;
     - resolved conflicts with package replacement;
    
    * chapter-2:
     - removed package associated with chapter;
     - resolved conflicts with package replacement;
    
    * 2-chapter:
     - HomeController.java controller replaced with view controller in WebConfig.java config class;
     - Finished 2-chapter;

--- taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
